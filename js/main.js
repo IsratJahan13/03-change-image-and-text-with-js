d27e617951d815a69d633cdbf38f1fd0f89cde9f
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-01-24 10:19:39 +0200

    03 projekti on valmis

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
 // const myHeading = document.querySelector("h1");
 // myHeading.textContent = "Hello world!";
 
-const myImage = document.querySelector("img")
+const myImage = document.querySelector("img");
+let myHeading = document.querySelector("h1");
+const myButton = document.querySelector("button");
+let myContent = "Mozilla is cool";
+let myName = "";
 
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src")
     if(mySrc === "./img/firefox-icon.png") {
         myImage.setAttribute("src", "./img/Brave_icon_lionface.png");
         document.body.style.backgroundColor = "white";
-
+        myContent = "Is Brave cooler?"
+        // myHeading.textContent = `${myContent}, ${myName}`;
+        myHeading.innerHTML = myContent;
+        if(myName) {
+            myHeading.innerHTML = myContent + ", " + myName;
+        }
+        
     } else {
         myImage.setAttribute("src", "./img/firefox-icon.png");
         document.body.style.backgroundColor = "orange";
+        myContent = "Mozilla is cool";
+        myHeading.innerHTML = myContent;
+        if (myName) {
+        myHeading.innerHTML = myContent + ", " + myName;
+        }
     }
-
 }
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
+function setUserName () {
+    myName = prompt("Please enter your name.");
 
     if (!myName) {
         setUserName();
       } else {
         localStorage.setItem("name", myName);
-        myHeading.textContent = `Mozilla is cool, ${myName}`;
+        myHeading.innerHTML = myContent + ", " + myName;
+        // myHeading.textContent = `${myContent}, ${myName}`;
+        
     }
     
 }
@@ -34,3 +47,6 @@ function setUserName() {
 myButton.onclick = () => {
     setUserName();
 }
+
+
+
